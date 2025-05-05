d5659d2b022581afc52a5bc1aa8327725f1cf881
Lawrence Cheung <15526007+lawrenceccheung@users.noreply.github.com>
2025-05-05 02:54:14 -0700

    Fix bugs

--- README.md
+++ README.md
@@ -13,17 +13,11 @@ $$
 
 $$
 u \frac{\partial u}{\partial x} + v \frac{\partial u}{\partial y} + w \frac{\partial u}{\partial z} = 
-\nu \left( \frac{\partial^2 u}{\partial y^2} +  \frac{\partial^2 u}{\partial z^2} \right)
+\nu \left( \frac{\partial^2 u}{\partial y^2} +  \frac{\partial^2 u}{\partial z^2} \right) + f_x
 $$
 
-<!--
 $$
-u \frac{\partial v}{\partial x} + v \frac{\partial v}{\partial y} + w \frac{\partial v}{\partial z} =  \nu \left( \frac{\partial^2 v}{\partial y^2} +  \frac{\partial^2 v}{\partial z^2} \right)
-$$
--->
-
-$$
-u \frac{\partial w}{\partial x} + v \frac{\partial w}{\partial y} + w \frac{\partial w}{\partial z} = \nu \left( \frac{\partial^2 w}{\partial y^2} +  \frac{\partial^2 w}{\partial z^2} \right)
+u \frac{\partial w}{\partial x} + v \frac{\partial w}{\partial y} + w \frac{\partial w}{\partial z} = \nu \left( \frac{\partial^2 w}{\partial y^2} +  \frac{\partial^2 w}{\partial z^2} \right) + f_z
 $$
 
 ### Discretization
@@ -56,37 +50,37 @@ $$
 
 $$
 \tilde{u} \frac{u_{ij}^{n+1/2} - u_{ij}^n}{\Delta x/2} + \tilde{v} \frac{D_y u_{ij}^{n+1/2}}{\Delta y}  + \tilde{w} \frac{D_z u_{ij}^n }{\Delta z} = 
-\nu \left[ \frac{D_y^2 u_{ij}^{n+1/2}}{(\Delta y)^2} + \frac{D_z^2 u_{ij}^n}{(\Delta z)^2} \right]
+\nu \left[ \frac{D_y^2 u_{ij}^{n+1/2}}{(\Delta y)^2} + \frac{D_z^2 u_{ij}^n}{(\Delta z)^2} \right] + f_x^{n}
 $$
 
 $$
 \tilde{u} \frac{u_{ij}^{n+1/2}}{\Delta x/2} + \tilde{v} \frac{D_y u_{ij}^{n+1/2}}{\Delta y} + \nu \frac{D_y^2 u_{ij}^{n+1/2} }{(\Delta y)^2} =   
-\tilde{u} \frac{u_{ij}^n}{\Delta x/2} - \tilde{w} \frac{D_z u_{ij}^n }{\Delta z} + \nu \frac{D_z^2 u_{ij}^n}{(\Delta z)^2}
+\tilde{u} \frac{u_{ij}^n}{\Delta x/2} - \tilde{w} \frac{D_z u_{ij}^n }{\Delta z} + \nu \frac{D_z^2 u_{ij}^n}{(\Delta z)^2} + f_x^{n}
 $$
 
 Reorganizing
 
 $$ \left[
 \frac{ \tilde{u}_{ij}}{\Delta x/2} + \tilde{v}_{ij} \frac{D_y}{\Delta y} + \nu \frac{D_y^2 }{(\Delta y)^2} \right] u_{ij}^{n+1/2} =
-\left[  \frac{ \tilde{u}_{ij} }{\Delta x/2} - \tilde{w}_{ij} \frac{D_z }{\Delta z} + \nu \frac{D_z^2}{(\Delta z)^2} \right] u_{ij}^n
+\left[  \frac{ \tilde{u}_{ij} }{\Delta x/2} - \tilde{w}_{ij} \frac{D_z }{\Delta z} + \nu \frac{D_z^2}{(\Delta z)^2} \right] u_{ij}^n + f_x^n
 $$
 
 #### U-momentum second step
 
 $$
 \tilde{u} \frac{u_{ij}^{n+1} - u_{ij}^{n+1/2}}{\Delta x/2} + \tilde{v} \frac{D_y u_{ij}^{n+1/2}}{\Delta y} + \tilde{w} \frac{D_z u_{ij}^{n+1} }{\Delta z} = 
-\nu \left[ \frac{D_y^2 u_{ij}^{n+1/2}}{(\Delta y)^2} + \frac{D_z^2 u_{ij}^{n+1}}{(\Delta z)^2} \right]
+\nu \left[ \frac{D_y^2 u_{ij}^{n+1/2}}{(\Delta y)^2} + \frac{D_z^2 u_{ij}^{n+1}}{(\Delta z)^2} \right] + f_x^{n+1/2}
 $$
 
 $$
 \tilde{u} \frac{u_{ij}^{n+1}}{\Delta x/2} + \tilde{w} \frac{D_z u_{ij}^{n+1} }{\Delta z} - \nu \frac{D_z^2 u_{ij}^{n+1}}{(\Delta z)^2}=
-\tilde{u} \frac{u_{ij}^{n+1/2}}{\Delta x/2} - \tilde{v} \frac{D_y u_{ij}^{n+1/2}}{\Delta y} + \nu \frac{D_y^2 u_{ij}^{n+1/2}}{(\Delta y)^2}
+\tilde{u} \frac{u_{ij}^{n+1/2}}{\Delta x/2} - \tilde{v} \frac{D_y u_{ij}^{n+1/2}}{\Delta y} + \nu \frac{D_y^2 u_{ij}^{n+1/2}}{(\Delta y)^2} + f_x^{n+1/2}
 $$
 
 Reorganizing
 
 $$ \left[\frac{\tilde{u}_{ij}}{\Delta x/2} + \tilde{w}_{ij} \frac{D_z }{\Delta z} - \nu \frac{D_z^2 }{(\Delta z)^2} \right] u_{ij}^{n+1}=
-\left[ \frac{\tilde{u}_{ij}}{\Delta x/2} - \tilde{v}_{ij} \frac{D_y }{\Delta y} + \nu \frac{D_y^2 }{(\Delta y)^2} \right] u_{ij}^{n+1/2}
+\left[ \frac{\tilde{u}_{ij}}{\Delta x/2} - \tilde{v}_{ij} \frac{D_y }{\Delta y} + \nu \frac{D_y^2 }{(\Delta y)^2} \right] u_{ij}^{n+1/2} + f_x^{n+1/2}
 $$
 
 #### W-momentum first step
--- SANDwake3D_laminar.py
+++ SANDwake3D_laminar.py
@@ -2,6 +2,8 @@
 
 import numpy as np
 from scipy.linalg import solve_banded
+import copy
+from collections import OrderedDict
 
 def solvetridiag(matrow, b, verbose=False):
     """
@@ -21,11 +23,37 @@ def solvetridiag(matrow, b, verbose=False):
 
 # Set up differentiation operators on the RHS
 # ==================================
+# first derivative
+def D1yfor(u, i, j):
+    return (u[i+1, j] - u[i, j])
+
+def D1zfor(u, i, j):
+    return (u[i, j+1] - u[i, j])
+
+def D1yback(u, i, j):
+    return (u[i, j] - u[i-1, j])
+
+def D1zback(u, i, j):
+    return (u[i, j] - u[i, j-1])
+
 def D1y(u, i, j):
     return 0.5*(u[i+1, j] - u[i-1, j])
 
 def D1z(u, i, j):
     return 0.5*(u[i, j+1] - u[i, j-1])
+
+# second derivative
+def D2yfor(u, i, j):
+    return u[i, j] - 2.0*u[i+1, j] + u[i+2, j]
+
+def D2zfor(u, i, j):
+    return u[i, j] - 2.0*u[i, j+1] + u[i, j+2]
+
+def D2yback(u, i, j):
+    return u[i, j] - 2.0*u[i-1, j] + u[i-2, j]
+
+def D2zback(u, i, j):
+    return u[i, j] - 2.0*u[i, j-1] + u[i, j-2]
 
 def D2y(u, i, j):
     return u[i+1, j] - 2.0*u[i, j] + u[i-1, j]
@@ -40,7 +68,8 @@ def applyBC(bcdict, location, dz, z=None):
     D1for  = np.array([0,  -1,  1])/(dz)
     D1back = np.array([-1,  1,  0])/(dz)
     Dstencil =  D1for if location=='lower' else D1back
-    value = bcdict['func'](z) if bcdict['value'] is None else bcdict['value']
+    #value = bcdict['func'](z) if bcdict['value'] is None else bcdict['value']
+    value  = bcdict['value']
     
     if bcdict['type'] == 'dirichlet':
         return Irow,  value
@@ -77,18 +106,18 @@ def RHS_u_nhalf(phi_np1, phi_n, dx, dy, dz, params):
     Nz = N[1]
     RHS = np.zeros((Ny, Nz))
     # These loops can be optimized
-    for i in range(1,Ny-1):
+    for i in range(Ny):
+        j=0
+        RHS[i,j] = u_tilde[i,j]*u_n[i,j]/(0.5*dx) - w_tilde[i,j]/dz*D1zfor(u_n, i, j) + nu*D2zfor(u_n, i, j)/(dz*dz)  
         for j in range(1,Nz-1):
             RHS[i,j] = u_tilde[i,j]*u_n[i,j]/(0.5*dx) - w_tilde[i,j]/dz*D1z(u_n, i, j) + nu*D2z(u_n, i, j)/(dz*dz)
+        j=Nz-1
+        RHS[i,j] = u_tilde[i,j]*u_n[i,j]/(0.5*dx) - w_tilde[i,j]/dz*D1zback(u_n, i, j) + nu*D2zback(u_n, i, j)/(dz*dz)
     return RHS
 
-def RHS_u_np1(phi_np1, phi_nhalf, phi_n, dx, dy, dz, params):
+def RHS_u_np1(phi_np1, u_nhalf, phi_n, dx, dy, dz, params):
     """
     """
-    u_nhalf = phi_nhalf['u']
-    v_nhalf = phi_nhalf['v']
-    w_nhalf = phi_nhalf['w']
-
     u_np1, u_n = phi_np1['u'], phi_n['u']
     v_np1, v_n = phi_np1['v'], phi_n['v']
     w_np1, w_n = phi_np1['w'], phi_n['w']
@@ -101,9 +130,13 @@ def RHS_u_np1(phi_np1, phi_nhalf, phi_n, dx, dy, dz, params):
     Nz = N[1]
     RHS = np.zeros((Ny, Nz))
     # These loops can be optimized
-    for i in range(1,Ny-1):
-        for j in range(1,Nz-1):
+    for j in range(Nz):
+        i=0
+        RHS[i,j] = u_tilde[i,j]*u_nhalf[i,j]/(0.5*dx) - v_tilde[i,j]/dy*D1yfor(u_nhalf, i, j) + nu*D2yfor(u_nhalf, i, j)/(dy*dy)
+        for i in range(1,Ny-1):
             RHS[i,j] = u_tilde[i,j]*u_nhalf[i,j]/(0.5*dx) - v_tilde[i,j]/dy*D1y(u_nhalf, i, j) + nu*D2y(u_nhalf, i, j)/(dy*dy)
+        i=Ny-1
+        RHS[i,j] = u_tilde[i,j]*u_nhalf[i,j]/(0.5*dx) - v_tilde[i,j]/dy*D1yback(u_nhalf, i, j) + nu*D2yback(u_nhalf, i, j)/(dy*dy) 
     return RHS
 
 def advanceU(phi_np1old, phi_n, dx, dy, dz, params, bc_ylo, bc_yhi, bc_zlo, bc_zhi):
@@ -117,7 +150,8 @@ def advanceU(phi_np1old, phi_n, dx, dy, dz, params, bc_ylo, bc_yhi, bc_zlo, bc_z
     Nz = N[1]
 
     # Load parameters
-    nu = params['nu']
+    nu       = params['nu']
+    fx_const = params['fx_const'] if 'fx_const' in params else 0.0
     
     # --- differentiation stencils ---
     #                  j-1  j  j+1
@@ -129,12 +163,12 @@ def advanceU(phi_np1old, phi_n, dx, dy, dz, params, bc_ylo, bc_yhi, bc_zlo, bc_z
     # First sweep: n -> n+1/2
     # -----------------------
     u_nhalf   = np.zeros((Ny, Nz))
-    RHS_nhalf = RHS_u_nhalf(phi_np1old, phi_n, dx, dy, dz, params)
+    RHS_nhalf = RHS_u_nhalf(phi_np1old, phi_n, dx, dy, dz, params) + fx_const
     for j in range(Nz):
         LHS_nhalf = np.zeros((Ny,3))
         # == Set up the LHS matrices ==
         for i in range(1,Ny-1):
-            LHS_nhalf[i,:] = u_tilde[i,j]/(0.5*dx) + v_tilde[i,j]*Dcen/dy + nu*D2cen/(dy*dy)
+            LHS_nhalf[i,:] = u_tilde[i,j]/(0.5*dx)*Irow + v_tilde[i,j]*Dcen/dy + nu*D2cen/(dy*dy)
         # Apply BC's
         row_lo, dentry_lo = applyBC(bc_ylo, 'lower', dy)
         row_hi, dentry_hi = applyBC(bc_yhi, 'upper', dy)
@@ -142,28 +176,31 @@ def advanceU(phi_np1old, phi_n, dx, dy, dz, params, bc_ylo, bc_yhi, bc_zlo, bc_z
         LHS_nhalf[-1,:] = row_hi
         RHS_nhalf[0,:]  = dentry_lo
         RHS_nhalf[-1,:] = dentry_hi
+        #print(f'j = {j}\nRHS_nhalf = ',RHS_nhalf[:,j], '\nLHS = ', LHS_nhalf)        
         # Solve the triadiagonal system
         u_nhalf[:,j] = solvetridiag(LHS_nhalf, RHS_nhalf[:,j])
 
+    print(f'u_nhalf = ',u_nhalf)
     # Second sweep: n+1/2 -> n+1
     # -----------------------
     u_np1   = np.zeros((Ny, Nz))
-    RHS_np1 = RHS_u_np1(u_nhalf, dx, dy, alpha)
+    RHS_np1 = RHS_u_np1(phi_np1old, u_nhalf, phi_n, dx, dy, dz, params) + fx_const
     # == Set up the LHS matrices ==
     for i in range(Ny):
         LHS_np1 = np.zeros((Nz,3))
-        for i in range(1,Nz-1):
-            LHS_np1[i,:] = u_tilde[i,j]/(0.5*dx) + w_tilde[i,j]*Dcen/dz + nu*D2cen/(dz*dz) 
+        for j in range(1,Nz-1):
+            LHS_np1[j,:] = u_tilde[i,j]/(0.5*dx)*Irow + w_tilde[i,j]*Dcen/dz + nu*D2cen/(dz*dz) 
         # Apply BC's
         row_lo, dentry_lo = applyBC(bc_zlo, 'lower', dz)
         row_hi, dentry_hi = applyBC(bc_zhi, 'upper', dz)
         LHS_np1[0,:]  = row_lo
         LHS_np1[-1,:] = row_hi
-        RHS_np1[0,:]  = dentry_lo
-        RHS_np1[-1,:] = dentry_hi
+        RHS_np1[:,0]  = dentry_lo
+        RHS_np1[:,-1] = dentry_hi
+        #print(f'i = {i}\nRHS_np1 = ',RHS_nhalf[i,:], '\nLHS = ', LHS_np1)                
         # Solve the triadiagonal system
         u_np1[i,:] = solvetridiag(LHS_np1, RHS_np1[i,:], verbose=False)
-
+    print('u_np1 = ',u_np1)
     return u_np1
 
 def advanceW(phi_np1old, phi_n, dx, dy, dz, params, bc_ylo, bc_yhi, bc_zlo, bc_zhi):
@@ -176,6 +213,7 @@ def advanceW(phi_np1old, phi_n, dx, dy, dz, params, bc_ylo, bc_yhi, bc_zlo, bc_z
     Ny = N[0]
     Nz = N[1]
     return np.zeros((Ny, Nz))
+
 
 def advanceMass(phi_np1old, phi_n, dx, dy, dz, params, bc_ylo, bc_yhi, bc_zlo, bc_zhi):
     """
@@ -213,19 +251,69 @@ def advanceMass(phi_np1old, phi_n, dx, dy, dz, params, bc_ylo, bc_yhi, bc_zlo, b
 
     # == Set up the LHS matrices ==
     for j in range(Nz):
-        LHS = np.zeros((Ny,3))
-        # == Set up the LHS matrices ==
-        for i in range(1,Ny-1):
-            LHS[i,:] = Dcen
-        # Apply BC's
-        row_lo, dentry_lo = applyBC(bc_ylo, 'lower', dy)
-        row_hi, dentry_hi = applyBC(bc_yhi, 'upper', dy)
-        LHS[0,:]  = row_lo
-        LHS[-1,:] = row_hi
-        RHS[0,:]  = dentry_lo
-        RHS[-1,:] = dentry_hi
-        # Solve the triadiagonal system
-        v_np1[:,j] = solvetridiag(LHS, RHS[:,j])
+        v_np1[:,j] = np.cumsum(RHS[:,j])
     
-    
+    # # == Set up the LHS matrices ==
+    # for j in range(Nz):
+    #     LHS = np.zeros((Ny,3))
+    #     # == Set up the LHS matrices ==
+    #     for i in range(1,Ny-1):
+    #         LHS[i,:] = Dcen
+    #     # Apply BC's
+    #     row_lo, dentry_lo = applyBC(bc_ylo, 'lower', dy)
+    #     row_hi, dentry_hi = applyBC(bc_yhi, 'upper', dy)
+    #     LHS[0,:]  = row_lo
+    #     LHS[-1,:] = row_hi
+    #     RHS[0,:]  = dentry_lo
+    #     RHS[-1,:] = dentry_hi
+    #     # Solve the triadiagonal system
+    #     v_np1[:,j] = solvetridiag(LHS, RHS[:,j])
+        
     return v_np1
+
+def convergetest(phi_new, phi_old, tol):
+    """
+    """
+    varlist = [v for v, g in phi_old.items()]
+    convergevar = {}
+    for v in varlist:
+        convergevar[v] = np.linalg.norm(phi_new[v] - phi_old[v])
+    converged = True
+    for v in varlist:
+        if (convergevar[v] > tol):
+            converged = False
+            break
+    return converged, convergevar
+
+def advanceSystem(phi_n, dx, dy, dz, params, allbcs, eqnsys, maxiter=100,
+                  tol=1.0E-6, verbose=False):
+    """
+    Advance equation system 1 step in x
+    """
+    varlist = [v for v, g in eqnsys.items()]
+
+    phi_n1 = copy.deepcopy(phi_n)
+
+    for k in range(maxiter):
+        phi_next = {}
+        # Loop over all variables
+        for v in varlist:
+            bcvar = allbcs[v]
+            phi_next[v] = eqnsys[v](phi_n1, phi_n, dx, dy, dz, params,
+                                    bcvar['ylo'], bcvar['yhi'], bcvar['zlo'], bcvar['zhi'])
+        # Test for convergence
+        converged, convergedat = convergetest(phi_next, phi_n1, tol)
+        phi_n1 = copy.deepcopy(phi_next)
+        if verbose:
+            print(k, convergedat)
+        if converged:
+            break
+    # Check if k hit maxiter:
+    # -->TODO!
+    return phi_n1
+
+# Define the laminar equation system
+laminar_eqns=OrderedDict()
+laminar_eqns['u'] = advanceU
+laminar_eqns['w'] = advanceW
+laminar_eqns['v'] = advanceMass
