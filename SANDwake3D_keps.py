672b2fe6ef3c74bfdd5627884465307a30de9d58
Marc Henry de Frahan <marc.henrydefrahan@nrel.gov>
2025-06-18 12:58:15 -0600

    remove a bunch of code

--- SANDwake3D_keps.py
+++ SANDwake3D_keps.py
@@ -5,44 +5,46 @@ from scipy.linalg import solve_banded
 import copy
 from collections import OrderedDict
 from SANDwake3D_base import *
+from functools import partial
 
-def RHS_u_nhalf(phi_np1, phi_n, Dz_nuT_tilde, dx, dy, dz, params):
+
+def RHS_f_nhalf(phi_np1, phi_n, Dz_nuT_tilde, nuT_scale, dx, dy, dz, params, field):
     """
-    Go from n to n+1/2 for the u-momentum equation
+    Go from n to n+1/2
     """
-    u_n = phi_n['u']
+    vel_n = phi_n[field]
     phi_tilde = getTildeVars(phi_np1, phi_n)
     u_tilde, w_tilde = phi_tilde['u'], phi_tilde['w']
-    nu_total = params['nu'] + phi_tilde['nuT']
-    w_total  = w_tilde - Dz_nuT_tilde
+    nu_total = params['nu'] + phi_tilde['nuT'] / nuT_scale
+    w_total  = w_tilde - Dz_nuT_tilde  / nuT_scale
 
     inv_dx = 1.0 / dx
     inv_dz = 1.0 / dz
     inv_dz2 = 1.0 / (dz * dz)
     inv_half_dx = 2.0 * inv_dx
 
-    RHS = np.empty_like(u_n)
+    RHS = np.empty_like(vel_n)
     RHS[:, 0] = (
-        u_tilde[:, 0] * u_n[:, 0] * inv_half_dx
-        - w_total[:, 0] * inv_dz * (u_n[:, 1] - u_n[:, 0])
-        + nu_total[:, 0] * inv_dz2 * (u_n[:, 0] - 2.0 * u_n[:, 1] + u_n[:, 2])
+        u_tilde[:, 0] * vel_n[:, 0] * inv_half_dx
+        - w_total[:, 0] * inv_dz * (vel_n[:, 1] - vel_n[:, 0])
+        + nu_total[:, 0] * inv_dz2 * (vel_n[:, 0] - 2.0 * vel_n[:, 1] + vel_n[:, 2])
     )
 
     RHS[:, 1:-1] = (
-        u_tilde[:, 1:-1] * u_n[:, 1:-1] * inv_half_dx
-        - w_total[:, 1:-1] * inv_dz * 0.5 * (u_n[:, 2:] - u_n[:, :-2])
-        + nu_total[:, 1:-1] * inv_dz2 * (u_n[:, 2:] - 2.0 * u_n[:, 1:-1] + u_n[:, :-2])
+        u_tilde[:, 1:-1] * vel_n[:, 1:-1] * inv_half_dx
+        - w_total[:, 1:-1] * inv_dz * 0.5 * (vel_n[:, 2:] - vel_n[:, :-2])
+        + nu_total[:, 1:-1] * inv_dz2 * (vel_n[:, 2:] - 2.0 * vel_n[:, 1:-1] + vel_n[:, :-2])
     )
 
     RHS[:, -1] = (
-        u_tilde[:, -1] * u_n[:, -1] * inv_half_dx
-        - w_total[:, -1] * inv_dz * (u_n[:, -1] - u_n[:, -2])
-        + nu_total[:, -1] * inv_dz2 * (u_n[:, -1] - 2.0 * u_n[:, -2] + u_n[:, -3])
+        u_tilde[:, -1] * vel_n[:, -1] * inv_half_dx
+        - w_total[:, -1] * inv_dz * (vel_n[:, -1] - vel_n[:, -2])
+        + nu_total[:, -1] * inv_dz2 * (vel_n[:, -1] - 2.0 * vel_n[:, -2] + vel_n[:, -3])
     )
 
     return RHS
 
-def RHS_u_np1(phi_np1, u_nhalf, phi_n, Dy_nuT_tilde, dx, dy, dz, params):
+def RHS_f_np1(phi_np1, f_nhalf, phi_n, Dy_nuT_tilde, nuT_scale, dx, dy, dz, params):
     """
     Go from n+1/2 to n+1 for the u-momentum equation
     """
@@ -55,29 +57,29 @@ def RHS_u_np1(phi_np1, u_nhalf, phi_n, Dy_nuT_tilde, dx, dy, dz, params):
     inv_dy = 1.0 / dy
     inv_dy2 = 1.0 / (dy * dy)
 
-    RHS = np.empty_like(u_nhalf)
+    RHS = np.empty_like(f_nhalf)
     RHS[0, :] = (
-    u_tilde[0, :] * u_nhalf[0, :] * inv_half_dx
-    - v_total[0, :] * inv_dy * (u_nhalf[1, :] - u_nhalf[0, :])
-    + nu_total[0, :] * inv_dy2 * (u_nhalf[0, :] - 2.0 * u_nhalf[1, :] + u_nhalf[2, :])
+    u_tilde[0, :] * f_nhalf[0, :] * inv_half_dx
+    - v_total[0, :] * inv_dy * (f_nhalf[1, :] - f_nhalf[0, :])
+    + nu_total[0, :] * inv_dy2 * (f_nhalf[0, :] - 2.0 * f_nhalf[1, :] + f_nhalf[2, :])
     )
 
     RHS[1:-1, :] = (
-        u_tilde[1:-1, :] * u_nhalf[1:-1, :] * inv_half_dx
-        - v_total[1:-1, :] * inv_dy * 0.5 * (u_nhalf[2:, :] - u_nhalf[:-2, :])
-        + nu_total[1:-1, :] * inv_dy2 * (u_nhalf[2:, :] - 2.0 * u_nhalf[1:-1, :] + u_nhalf[:-2, :])
+        u_tilde[1:-1, :] * f_nhalf[1:-1, :] * inv_half_dx
+        - v_total[1:-1, :] * inv_dy * 0.5 * (f_nhalf[2:, :] - f_nhalf[:-2, :])
+        + nu_total[1:-1, :] * inv_dy2 * (f_nhalf[2:, :] - 2.0 * f_nhalf[1:-1, :] + f_nhalf[:-2, :])
     )
 
     RHS[-1, :] = (
-        u_tilde[-1, :] * u_nhalf[-1, :] * inv_half_dx
-        - v_total[-1, :] * inv_dy * (u_nhalf[-1, :] - u_nhalf[-2, :])
-        + nu_total[-1, :] * inv_dy2 * (u_nhalf[-1, :] - 2.0 * u_nhalf[-2, :] + u_nhalf[-3, :])
+        u_tilde[-1, :] * f_nhalf[-1, :] * inv_half_dx
+        - v_total[-1, :] * inv_dy * (f_nhalf[-1, :] - f_nhalf[-2, :])
+        + nu_total[-1, :] * inv_dy2 * (f_nhalf[-1, :] - 2.0 * f_nhalf[-2, :] + f_nhalf[-3, :])
     )
     return RHS
 
-def advanceU(phi_np1old, phi_n, dx, dy, dz, params, bc_ylo, bc_yhi, bc_zlo, bc_zhi):
+def advanceF(phi_np1old, phi_n, dx, dy, dz, params, bc_ylo, bc_yhi, bc_zlo, bc_zhi, field):
     """
-    Advance the u-momentum one full step
+    Advance the field one full step
     """
     phi_tilde = getTildeVars(phi_np1old, phi_n)
     u_tilde, v_tilde, w_tilde = phi_tilde['u'], phi_tilde['v'], phi_tilde['w']
@@ -107,188 +109,63 @@ def advanceU(phi_np1old, phi_n, dx, dy, dz, params, bc_ylo, bc_yhi, bc_zlo, bc_z
 
     # First sweep: n -> n+1/2
     # -----------------------
-    u_nhalf   = np.zeros((Ny, Nz))
-    RHS_nhalf = RHS_u_nhalf(phi_np1old, phi_n, Dz_nuT, dx, dy, dz, params) + fx_const
+    f_nhalf   = np.zeros((Ny, Nz))
+    RHS_nhalf = RHS_f_nhalf(phi_np1old, phi_n, Dz_nuT, 1.0, dx, dy, dz, params, field) + fx_const
+    inv_half_dx = 2.0 / dx
+    inv_dy = 1.0 / dy
+    inv_dy2 = 1.0 / (dy * dy)
+    inv_dz = 1.0 / dz
+    inv_dz2 = 1.0 / (dz * dz)
+    row_lo, dentry_lo = applyBC(bc_ylo, 'lower', dy)
+    row_hi, dentry_hi = applyBC(bc_yhi, 'upper', dy)
     for j in range(Nz):
         LHS_nhalf = np.zeros((Ny,3))
         # == Set up the LHS matrices ==
-        for i in range(1,Ny-1):
-            LHS_nhalf[i,:] = u_tilde[i,j]/(0.5*dx)*Irow + v_total[i,j]*Dcen/dy - nu_total[i,j]*D2cen/(dy*dy)
+        LHS_nhalf[1:-1, :] = (
+            u_tilde[1:-1, j, np.newaxis] * inv_half_dx * Irow
+            + v_total[1:-1, j, np.newaxis] * inv_dy * Dcen
+            - nu_total[1:-1, j, np.newaxis] * inv_dy2 * D2cen
+        )
         # Apply BC's
-        row_lo, dentry_lo = applyBC(bc_ylo, 'lower', dy)
-        row_hi, dentry_hi = applyBC(bc_yhi, 'upper', dy)
         LHS_nhalf[0,:]  = row_lo
         LHS_nhalf[-1,:] = row_hi
         RHS_nhalf[0,:]  = dentry_lo
         RHS_nhalf[-1,:] = dentry_hi
         #print(f'j = {j}\nRHS_nhalf = ',RHS_nhalf[:,j], '\nLHS = ', LHS_nhalf)        
         # Solve the triadiagonal system
-        u_nhalf[:,j] = solvetridiag(LHS_nhalf, RHS_nhalf[:,j])
+        f_nhalf[:,j] = solvetridiag(LHS_nhalf, RHS_nhalf[:,j])
 
-    #print(f'u_nhalf = ',u_nhalf)
+    #print(f'f_nhalf = ',f_nhalf)
     # Second sweep: n+1/2 -> n+1
     # -----------------------
-    u_np1   = np.zeros((Ny, Nz))
-    RHS_np1 = RHS_u_np1(phi_np1old, u_nhalf, phi_n, Dy_nuT, dx, dy, dz, params) + fx_const
+    f_np1   = np.zeros((Ny, Nz))
+    RHS_np1 = RHS_f_np1(phi_np1old, f_nhalf, phi_n, Dy_nuT, 1.0, dx, dy, dz, params) + fx_const
     # == Set up the LHS matrices ==
+    row_lo_base, dentry_lo_base = applyBC(bc_zlo, 'lower', dz)
+    row_hi_base, dentry_hi_base = applyBC(bc_zhi, 'upper', dz)
     for i in range(Ny):
         LHS_np1 = np.zeros((Nz,3))
-        for j in range(1,Nz-1):
-            LHS_np1[j,:] = u_tilde[i,j]/(0.5*dx)*Irow + w_total[i,j]*Dcen/dz - nu_total[i,j]*D2cen/(dz*dz) 
+        LHS_np1[1:-1, :] = (
+            u_tilde[i, 1:-1, np.newaxis] * inv_half_dx * Irow
+            + w_total[i, 1:-1, np.newaxis] * inv_dz * Dcen
+            - nu_total[i, 1:-1, np.newaxis] * inv_dz2 * D2cen
+        )
         # Apply BC's
-        row_lo, dentry_lo = applyBC(bc_zlo, 'lower', dz)
-        row_hi, dentry_hi = applyBC(bc_zhi, 'upper', dz)
+        row_lo, dentry_lo = row_lo_base, dentry_lo_base
+        row_hi, dentry_hi = row_hi_base, dentry_hi_base
         if i==0:
-            row_lo, dentry_lo = applyBC({'type':'dirichlet', 'value':u_tilde[i,0]}, 'lower', dz)
-        if i==Ny-1:
-            row_hi, dentry_hi = applyBC({'type':'dirichlet', 'value':u_tilde[i,-1]}, 'lower', dz)            
+            row_lo, dentry_lo = applyBC({'type':'dirichlet', 'value':phi_tilde[field][i,0]}, 'lower', dz)
+        elif i==Ny-1:
+            row_hi, dentry_hi = applyBC({'type':'dirichlet', 'value':phi_tilde[field][i,-1]}, 'lower', dz)            
         LHS_np1[0,:]  = row_lo
         LHS_np1[-1,:] = row_hi
         RHS_np1[:,0]  = dentry_lo
         RHS_np1[:,-1] = dentry_hi
         #print(f'i = {i}\nRHS_np1 = ',RHS_np1[i,:], '\nLHS = ', LHS_np1)                
         # Solve the triadiagonal system
-        u_np1[i,:] = solvetridiag(LHS_np1, RHS_np1[i,:], verbose=False)
-        #print('u_np1 = ',u_np1[i,:])
-    return u_np1
-
-def RHS_w_nhalf(phi_np1, phi_n, Dz_nuT_tilde, dx, dy, dz, params):
-    """
-    Go from n to n+1/2 for the w-momentum equation
-    """
-    u_np1, u_n = phi_np1['u'], phi_n['u']
-    v_np1, v_n = phi_np1['v'], phi_n['v']
-    w_np1, w_n = phi_np1['w'], phi_n['w']
-    phi_tilde = getTildeVars(phi_np1, phi_n)
-    u_tilde, v_tilde, w_tilde = phi_tilde['u'], phi_tilde['v'], phi_tilde['w']
-    nuT_tilde = phi_tilde['nuT']
-
-    nu = params['nu']
-
-    N  = u_n.shape
-    Ny = N[0]
-    Nz = N[1]
-    RHS = np.zeros((Ny, Nz))
-
-    nu_total = nu + nuT_tilde
-    w_total  = w_tilde - Dz_nuT_tilde
-
-    # These loops can be optimized
-    for i in range(Ny):
-        j=0
-        RHS[i,j] = u_tilde[i,j]*w_n[i,j]/(0.5*dx) - w_total[i,j]/dz*D1zfor(w_n, i, j) + nu_total[i,j]*D2zfor(w_n, i, j)/(dz*dz)  
-        for j in range(1,Nz-1):
-            RHS[i,j] = u_tilde[i,j]*w_n[i,j]/(0.5*dx) - w_total[i,j]/dz*D1z(w_n, i, j) + nu_total[i,j]*D2z(w_n, i, j)/(dz*dz)
-        j=Nz-1
-        RHS[i,j] = u_tilde[i,j]*w_n[i,j]/(0.5*dx) - w_total[i,j]/dz*D1zback(w_n, i, j) + nu_total[i,j]*D2zback(w_n, i, j)/(dz*dz)
-    return RHS
-
-def RHS_w_np1(phi_np1, w_nhalf, phi_n, Dy_nuT_tilde, dx, dy, dz, params):
-    """
-    Go from n+1/2 to n+1 for the w-momentum equation
-    """
-    u_np1, u_n = phi_np1['u'], phi_n['u']
-    v_np1, v_n = phi_np1['v'], phi_n['v']
-    w_np1, w_n = phi_np1['w'], phi_n['w']
-    phi_tilde = getTildeVars(phi_np1, phi_n)
-    u_tilde, v_tilde, w_tilde = phi_tilde['u'], phi_tilde['v'], phi_tilde['w']
-    nuT_tilde = phi_tilde['nuT']
-
-    nu = params['nu']
-
-    N  = u_n.shape
-    Ny = N[0]
-    Nz = N[1]
-    RHS = np.zeros((Ny, Nz))
-    
-    nu_total = nu + nuT_tilde
-    v_total  = v_tilde - Dy_nuT_tilde
-
-    # These loops can be optimized
-    for j in range(Nz):
-        i=0
-        RHS[i,j] = u_tilde[i,j]*w_nhalf[i,j]/(0.5*dx) - v_total[i,j]/dy*D1yfor(w_nhalf, i, j) + nu_total[i,j]*D2yfor(w_nhalf, i, j)/(dy*dy)
-        for i in range(1,Ny-1):
-            RHS[i,j] = u_tilde[i,j]*w_nhalf[i,j]/(0.5*dx) - v_total[i,j]/dy*D1y(w_nhalf, i, j) + nu_total[i,j]*D2y(w_nhalf, i, j)/(dy*dy)
-        i=Ny-1
-        RHS[i,j] = u_tilde[i,j]*w_nhalf[i,j]/(0.5*dx) - v_total[i,j]/dy*D1yback(w_nhalf, i, j) + nu_total[i,j]*D2yback(w_nhalf, i, j)/(dy*dy) 
-    return RHS
-
-def advanceW(phi_np1old, phi_n, dx, dy, dz, params, bc_ylo, bc_yhi, bc_zlo, bc_zhi):
-    """
-    Advance the W-momentum one full step
-    """
-    phi_tilde = getTildeVars(phi_np1old, phi_n)
-    u_tilde, v_tilde, w_tilde = phi_tilde['u'], phi_tilde['v'], phi_tilde['w']
-    nuT_tilde = phi_tilde['nuT']
-
-    N  = u_tilde.shape
-    Ny = N[0]
-    Nz = N[1]
-
-    # Load parameters
-    nu       = params['nu']
-    fz_const = params['fz_const'] if 'fz_const' in params else 0.0
-    
-    # --- differentiation stencils ---
-    #                  j-1  j  j+1
-    Irow   = np.array([0,   1,  0])
-    Dcen   = np.array([-1,  0,  1])*0.5
-    D2cen  = np.array([1,  -2,  1])
-    # -------------------------------
-
-    # Compute some quantities related to nuT
-    Dy_nuT, Dz_nuT = np.gradient(nuT_tilde, dy, dz, edge_order=1)
-
-    nu_total = nu + nuT_tilde
-    v_total  = v_tilde - Dy_nuT
-    w_total  = w_tilde - Dz_nuT
-
-    # First sweep: n -> n+1/2
-    # -----------------------
-    w_nhalf   = np.zeros((Ny, Nz))
-    RHS_nhalf = RHS_w_nhalf(phi_np1old, phi_n, Dz_nuT, dx, dy, dz, params) + fz_const
-    for j in range(Nz):
-        LHS_nhalf = np.zeros((Ny,3))
-        # == Set up the LHS matrices ==
-        for i in range(1,Ny-1):
-            LHS_nhalf[i,:] = u_tilde[i,j]/(0.5*dx)*Irow + v_total[i,j]*Dcen/dy - nu_total[i,j]*D2cen/(dy*dy)
-        # Apply BC's
-        row_lo, dentry_lo = applyBC(bc_ylo, 'lower', dy)
-        row_hi, dentry_hi = applyBC(bc_yhi, 'upper', dy)
-        LHS_nhalf[0,:]  = row_lo
-        LHS_nhalf[-1,:] = row_hi
-        RHS_nhalf[0,:]  = dentry_lo
-        RHS_nhalf[-1,:] = dentry_hi
-        # Solve the triadiagonal system
-        w_nhalf[:,j] = solvetridiag(LHS_nhalf, RHS_nhalf[:,j])
-
-    # Second sweep: n+1/2 -> n+1
-    # -----------------------
-    w_np1   = np.zeros((Ny, Nz))
-    RHS_np1 = RHS_w_np1(phi_np1old, w_nhalf, phi_n, Dy_nuT, dx, dy, dz, params) + fz_const
-    # == Set up the LHS matrices ==
-    for i in range(Ny):
-        LHS_np1 = np.zeros((Nz,3))
-        for j in range(1,Nz-1):
-            LHS_np1[j,:] = u_tilde[i,j]/(0.5*dx)*Irow + w_total[i,j]*Dcen/dz - nu_total[i,j]*D2cen/(dz*dz) 
-        # Apply BC's
-        row_lo, dentry_lo = applyBC(bc_zlo, 'lower', dz)
-        row_hi, dentry_hi = applyBC(bc_zhi, 'upper', dz)
-        if i==0:
-            row_lo, dentry_lo = applyBC({'type':'dirichlet', 'value':w_tilde[i,0]}, 'lower', dz)
-        if i==Ny-1:
-            row_hi, dentry_hi = applyBC({'type':'dirichlet', 'value':w_tilde[i,-1]}, 'lower', dz)            
-        LHS_np1[0,:]  = row_lo
-        LHS_np1[-1,:] = row_hi
-        RHS_np1[:,0]  = dentry_lo
-        RHS_np1[:,-1] = dentry_hi
-        #print(f'i = {i}\nRHS_np1 = ',RHS_np1[i,:], '\nLHS = ', LHS_np1)                
-        # Solve the triadiagonal system
-        w_np1[i,:] = solvetridiag(LHS_np1, RHS_np1[i,:], verbose=False)
-
-    return w_np1
-
+        f_np1[i,:] = solvetridiag(LHS_np1, RHS_np1[i,:], verbose=False)
+        #print('f_np1 = ',f_np1[i,:])
+    return f_np1
 
 def RHS_tke_nhalf(phi_np1, phi_n, Dz_nuT_tilde, dx, dy, dz, params):
     """
@@ -781,8 +658,8 @@ def set_k_init(rvec,dr,u,params,k_factor=0.1):
 ########################################################
 # Define the laminar equation system
 keps_eqns        = OrderedDict()
-keps_eqns['u']   = advanceU
-keps_eqns['w']   = advanceW
+keps_eqns['u']   = partial(advanceF, field="u")
+keps_eqns['w']   = partial(advanceF, field="w")
 keps_eqns['k']   = advanceTKE
 keps_eqns['eps'] = advanceEPS
 keps_eqns['v']   = advanceMass
